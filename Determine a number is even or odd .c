b519d72d82f7fa5549e814eb36eee634f681a328
David Mulwa <122589707+DavidMulwa@users.noreply.github.com>
2023-02-07 22:04:29 +0300

    Create Determine a number is even or odd .c

---
+++ Determine a number is even or odd .c
@@ -0,0 +1,12 @@
+/*program to determine if a number is odd or even*/
+#include <stdio.h>
+int main() {
+int a;
+printf("\n enter a value:");
+scanf("%d",&a);
+if(a%2==0){
+printf("a is even");
+}
+else{printf("a is odd");}
+return 0;
+}
