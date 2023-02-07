1db3036512ab3e01a7cbc0860dcb5fb43791d8cb
David Mulwa <122589707+DavidMulwa@users.noreply.github.com>
2023-02-07 22:11:01 +0300

    Create Determine  a number is positive or negative  .c

---
+++ Determine  a number is positive or negative  .c
@@ -0,0 +1,12 @@
+/*program to determine if a number is positive or negative*/
+#include <stdio.h>
+int main() {
+int a;
+printf("\n enter a value:");
+scanf("%d",&a);
+if(a>0){
+printf("a is positive");
+}
+else{printf("a is negative");}
+return 0;
+}
