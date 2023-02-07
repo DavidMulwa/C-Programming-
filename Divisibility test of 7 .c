ec63b1638122e89fbf885108a83bb3b09ce68a62
David Mulwa <122589707+DavidMulwa@users.noreply.github.com>
2023-02-07 21:53:46 +0300

    Create Divisibility test of 7 .c

---
+++ Divisibility test of 7 .c
@@ -0,0 +1,12 @@
+//divisibility test of 7
+#include <stdio.h>
+int main() {
+int a;
+printf("\n enter a value:");
+scanf("%d",&a);
+if(a%7==0){
+printf("a is divisible by 7");
+}
+else{printf("a is not divisible 7");}
+    return 0;
+}
