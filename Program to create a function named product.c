0f1ac5bd1a8ee57b69cbb25aae5f78e68b3b9199
David Mulwa <122589707+DavidMulwa@users.noreply.github.com>
2023-03-01 10:04:23 +0300

    Create Program to create a function named product.c

---
+++ Program to create a function named product.c
@@ -0,0 +1,15 @@
+//program to create a function named product 
+include<stdio.h>
+int product(int,int);
+int product(int,int);
+int main() {
+    int product;
+    int num1,num2;
+    printf("\nEnter value of num1");
+    printf("\nEnter value of num2");
+    scanf("%d%d",&num1,&num2);
+    return 0;
+}
+int product(int num1,int num2){
+    return num1*num2;
+}
